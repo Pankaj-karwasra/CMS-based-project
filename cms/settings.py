db72bee017358b132b8afc2c4e37afad51f53d7e
Pankaj karwasra <160117448+Pankaj-karwasra@users.noreply.github.com>
2024-11-20 12:40:24 +0530

    Update settings.py

--- cms/settings.py
+++ cms/settings.py
@@ -143,8 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pankajjaat913@gmail.com'
-EMAIL_HOST_PASSWORD = 'wyerbaoebmpbeptx'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 
